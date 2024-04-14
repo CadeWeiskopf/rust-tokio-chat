c74361a05df800890d789e2630b8cf45dd16b0f7
Cade Weiskopf <cadew@pm.me>
2024-04-14 01:52:55 -0400

    ids, error catch

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,53 @@
 use futures_util::{SinkExt, StreamExt};
-// use http::Uri;
 use tokio::net::TcpListener;
-use tokio_websockets::{Error, ServerBuilder};
+use tokio_websockets::{Error, Message, ServerBuilder};
+use uuid::Uuid;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
   let listener = TcpListener::bind("127.0.0.1:3000").await?;
 
   tokio::spawn(async move {
+    /*
+     * Spawn server start listening
+     */
     println!("Server listening... {:?}", listener); 
     while let Ok((stream, _)) = listener.accept().await { 
-      let mut ws_stream = ServerBuilder::new()
+      /*
+       * server setup client stream
+       */
+      let ws_stream_result = ServerBuilder::new()
         .accept(stream)
-        .await?;
-      println!("New connection to server");
+        .await;
+      let mut ws_stream = match ws_stream_result {
+        Ok(stream) => stream,
+        Err(err) => {
+          eprintln!("Failed to accept WebSocket connection: {}", err);
+          break;
+        }
+      };
+      let id = Uuid::new_v4();
+      println!("New connection to server {}", id);
+
+      /*
+       * server greet client and start listening/responding to messages
+       */
+      let greet_result = ws_stream
+        .send(Message::text("Welcome!".to_string()))
+        .await;
+      if let Err(err) = greet_result {
+        eprintln!("Failed to send greeting message: {}", err);
+      }
       tokio::spawn(async move {
         while let Some(Ok(msg)) = ws_stream.next().await {
           if msg.is_text() || msg.is_binary() {
-            println!("{:?}", msg);
-            ws_stream.send(msg).await?;
+            println!("{}: {:?}", id, msg);           
+            let response_result = ws_stream
+              .send(msg)
+              .await;
+            if let Err(err) = response_result {
+              eprintln!("Failed to send response message: {}", err);
+            }
           }
         }
         Ok::<_, Error>(())
