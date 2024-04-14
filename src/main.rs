f3479a6aad7d5ace0a7d61bd7b8560b16da6e143
Cade Weiskopf <cadew@pm.me>
2024-04-14 11:40:50 -0400

    json

--- src/main.rs
+++ src/main.rs
@@ -20,14 +20,16 @@ async fn main() -> Result<(), Error> {
         .accept(stream)
         .await;
       let mut ws_stream = match ws_stream_result {
-        Ok(stream) => stream,
+        Ok(stream) => {
+          stream
+        },
         Err(err) => {
           eprintln!("Failed to accept WebSocket connection: {}", err);
-          break;
+          break
         }
       };
       let id = Uuid::new_v4();
-      println!("New connection to server {}", id);
+      println!("New connection to server {} \n {:?} \n\n", id, ws_stream);
 
       /*
        * server greet client and start listening/responding to messages
@@ -40,13 +42,35 @@ async fn main() -> Result<(), Error> {
       }
       tokio::spawn(async move {
         while let Some(Ok(msg)) = ws_stream.next().await {
-          if msg.is_text() || msg.is_binary() {
-            println!("{}: {:?}", id, msg);           
-            let response_result = ws_stream
-              .send(msg)
-              .await;
-            if let Err(err) = response_result {
-              eprintln!("Failed to send response message: {}", err);
+          println!("{}: {:?}", id, msg);           
+          if msg.is_text() || msg.is_binary() {           
+            let msg_str_option = msg.as_text();
+            if let Some(msg_str) = msg_str_option {
+              let parse_json_result = parse_json_message(msg_str);
+              match parse_json_result {
+                Ok(mut value) => {
+                  value["id"] = serde_json::Value::String(id.to_string());
+                  println!("Do something {:?}", value);
+                  let response_str = match serde_json::to_string(&value) {
+                    Ok(value) => value,
+                    Err(err) => {
+                      eprintln!("Something went wrong creating response str: {}", err);
+                      "{\"error\": true}".to_string()
+                    }
+                  };
+                  let response_result = ws_stream
+                    .send(Message::text(response_str))
+                    .await;
+                  if let Err(err) = response_result {
+                    eprintln!("Failed to send response message: {}", err);
+                  }
+                },
+                Err(err) => {
+                  eprintln!("Error parsing json on received message: {}", err);
+                }                  
+              };
+            } else {
+              eprintln!("Err getting msg as text: {:?}", msg);
             }
           }
         }
@@ -61,4 +85,10 @@ async fn main() -> Result<(), Error> {
     tokio::time::sleep(tokio::time::Duration::from_secs(60)).await;
   }
 
+}
+
+fn parse_json_message(data: &str) -> serde_json::Result<serde_json::Value> {
+  println!("parsing {}", data);
+  let value: serde_json::Value = serde_json::from_str(data)?;
+  Ok(value)
 }
