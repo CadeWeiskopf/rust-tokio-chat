482d0a80d1fb40c8018d2a4f98ee13c605ca2f52
Cade Weiskopf <cadew@pm.me>
2024-04-14 23:11:56 -0400

    queueing messages

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
-use futures_util::{stream::SplitSink, SinkExt, StreamExt, TryStreamExt};
+use futures_util::{stream::SplitSink, SinkExt, StreamExt};
 use tokio::net::{TcpListener, TcpStream};
 use tokio_websockets::{Error, Message, ServerBuilder};
 use uuid::Uuid;
 use std::collections::HashMap;
 use std::sync::Arc;
-use tokio::sync::{Mutex, MutexGuard};
+use tokio::sync::Mutex;
+use serde_json::Value;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
   let listener = TcpListener::bind("127.0.0.1:3000").await?;
-  let mut clients: HashMap<Uuid, Arc<Mutex<SplitSink<tokio_websockets::WebSocketStream<TcpStream>, Message>>>> = HashMap::new();
-  let mut locks: HashMap<Uuid, MutexGuard<'static, tokio_websockets::WebSocketStream<tokio::net::TcpStream>>> = HashMap::new();  
+  let clients: HashMap<Uuid, Arc<Mutex<SplitSink<tokio_websockets::WebSocketStream<TcpStream>, Message>>>> = HashMap::new();
   let clients_map = Arc::new(Mutex::new(clients));
-
+  let messages_vec: Arc<Mutex<Vec<Value>>> = Arc::new(Mutex::new(Vec::new()));
   let clients_map_clone = clients_map.clone();
   let clients_map_clone2 = clients_map.clone();
 
@@ -33,7 +33,7 @@ async fn main() -> Result<(), Error> {
         }
       };
       
-      let (mut tx, mut rx) = ws_stream.split();
+      let (tx, rx) = ws_stream.split();
 
       let id = Uuid::new_v4();
       let client_rx_stream = Arc::new(Mutex::new(rx));
@@ -46,6 +46,7 @@ async fn main() -> Result<(), Error> {
       
       // spawn async task to listen for client messages
       // let clients_map_clone = clients_map.clone();
+      let msg_vec_clone = messages_vec.clone();
       tokio::spawn(async move {
         let client_rx_stream_clone = client_rx_stream.clone();
         loop {
@@ -53,9 +54,26 @@ async fn main() -> Result<(), Error> {
           match client_rx_lock.next().await {
             Some(Ok(msg)) => {
               println!("{}: {:?}", id, msg);
-              // client_lock.send(Message::text("hello".to_string())).await;
-              let mut client_tx_lock = client_tx_stream.lock().await;
-              client_tx_lock.send(Message::text("hello".to_string())).await;
+              match msg.as_text() {
+                Some(msg_str) => {
+                  match parse_json_message(msg_str) {
+                    Ok(msg_json) => {
+                      eprintln!("Parsed message json: {}", msg_json);
+                      let mut messages_vec_lock = msg_vec_clone.lock().await;
+                      messages_vec_lock.push(msg_json);
+                      println!("total messages = {}", messages_vec_lock.len());
+                      std::mem::drop(messages_vec_lock);
+                    },
+                    Err(err) => {
+                      eprintln!("Error parsing message as json: {}", err);
+                    }
+                  }   
+                },
+                None => {
+                  eprintln!("None message as text");
+                }
+              }
+              let client_tx_lock = client_tx_stream.lock().await;
               std::mem::drop(client_tx_lock);
             },
             Some(Err(err)) => {
@@ -77,16 +95,21 @@ async fn main() -> Result<(), Error> {
   tokio::spawn(async move {
     loop {
       tokio::time::sleep(tokio::time::Duration::from_secs(5)).await;
-      let mut clients_map_lock = clients_map_clone2.lock().await;
+      let clients_map_lock = clients_map_clone2.lock().await;
       for (id, client_stream) in clients_map_lock.iter() {
         println!("send {}", id);
         let mut c = client_stream.lock().await;
-        c.send(Message::text("hello".to_string())).await;
+        let send_msg_result = c.send(Message::text("hello".to_string())).await;
+        match send_msg_result {
+          Ok(()) => {},
+          Err(err) => {
+            eprintln!("err sending message to client {}: {}", id, err);
+          }
+        }
         std::mem::drop(c);
       }
       std::mem::drop(clients_map_lock);
     }  
-    Ok::<_, Error>(())
   });
 
   loop {
